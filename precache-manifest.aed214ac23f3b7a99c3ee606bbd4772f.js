9956d9c08d348a5512041cc06e933c68e964060c
Minh Hoang <minhhoang220676@gmail.com>
2020-12-04 08:47:03 +0700

    Updates

---
+++ precache-manifest.aed214ac23f3b7a99c3ee606bbd4772f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "560c32db4c12ef1d19a9ba2384dee7a8",
+    "url": "/login/index.html"
+  },
+  {
+    "revision": "268650202c1c7d6320fd",
+    "url": "/login/static/css/main.2cb127c3.chunk.css"
+  },
+  {
+    "revision": "6b241cde8b7f92373779",
+    "url": "/login/static/js/2.a2c60b78.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/login/static/js/2.a2c60b78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "268650202c1c7d6320fd",
+    "url": "/login/static/js/main.3d581d8b.chunk.js"
+  },
+  {
+    "revision": "2a6ddebc308cbef80bfa",
+    "url": "/login/static/js/runtime-main.eced9b5c.js"
+  }
+]);
